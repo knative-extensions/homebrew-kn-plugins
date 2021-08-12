7495e3a2728274b94068f13046289b2fc3ff1e57
Lionel Villard <villard@us.ibm.com>
2021-08-12 04:15:34 -0400

    release 0.25 (#29)

--- admin.rb
+++ admin.rb
@@ -1,7 +1,7 @@
 require 'fileutils'
 
 class Admin < Formula
-  v = "v0.24.0"
+  v = "v0.25.0"
   plugin_name = "admin"
   path_name = "kn-plugin-#{plugin_name}"
   file_name = "kn-#{plugin_name}"
@@ -13,10 +13,10 @@ class Admin < Formula
 
   if OS.mac?
     url "#{base_url}/#{file_name}-darwin-amd64"
-    sha256 "0a7e0a523ce7800e3459f5ed010aa392f293be55e2c664f2a23f76aafb5fbae4"
+    sha256 "42fa937b3e9255fc7bd0187211b28ee4081b20f5c5c2031d468c6ab8bb085089"
   else
     url "#{base_url}/#{file_name}-linux-amd64"
-    sha256 "ce3e123e6fdf9ea31bd861f3ed82261da48b6fc8d43bd3e57430b3e33063c16b"
+    sha256 "fced1d9a3a2775637e8997ba01bd21782176caac3fc7d2556cc68c593cd3dc86"
   end
 
   def install
