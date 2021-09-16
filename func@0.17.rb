e824101f11196652b618ee1956750a5aa977eee9
David Simansky <dsimansk@redhat.com>
2021-09-16 21:23:39 +0200

    Update kn-plugin-func formula to v0.18.0 (#35)

--- func@0.17.rb
+++ func@0.17.rb
@@ -1,7 +1,7 @@
 require 'fileutils'
 
-class Func < Formula
-  v = "v0.18.0"
+class FuncAT017 < Formula
+  v = "v0.17.1"
   plugin_name = "func"
   path_name = "kn-plugin-#{plugin_name}"
   file_name = "#{plugin_name}"
@@ -13,10 +13,10 @@ class Func < Formula
 
   if OS.mac?
     url "#{base_url}/#{file_name}_darwin_amd64.gz"
-    sha256 "3d1de7fe30cb7a21581f0dd7e554b19b7c343dc43ac555eff9512545fd78722c"
+    sha256 "f83d1ff15045cd882fedda4944581d5e3084a82d68a309b4c5c900af91968e11"
   else
     url "#{base_url}/#{file_name}_linux_amd64.gz"
-    sha256 "a332f007531c8ac1ef3fa73e330ea8682e84cffe9a2070dd3af7835380b8553f"
+    sha256 "2d324d0303605814f37deada0fe1317f1812875ba6bba901808b2ad69381723a"
   end
 
   def install
