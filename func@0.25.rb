fa1b80bcddbcf0b196b5f368e64507e384556d03
salaboy <msalatino@vmware.com>
2022-09-20 19:20:09 +0100

    Updating func to 1.7 release (#112)
    * updating to func 1.7.0
    
    * replacing shas
    
    * adding linux arm64

--- func@0.25.rb
+++ func@0.25.rb
@@ -1,7 +1,7 @@
 require 'fileutils'
 
 class Func < Formula
-  v = "knative-v1.7.0"
+  v = "v0.25.1"
   plugin_name = "func"
   path_name = "kn-plugin-#{plugin_name}"
   file_name = "#{plugin_name}"
@@ -14,21 +14,14 @@ class Func < Formula
   if OS.mac?
     if `uname -m`.chomp  == "arm64"
       url "#{base_url}/#{file_name}_darwin_arm64"
-      sha256 "d2ab0de9c3b058e1a27b0cb2cb8a2a630b6526d2e8de9571c9894813faabf69d"
+      sha256 "ba13659893f1e5dd114a4162f2c90ef246ef6e32c6dace22c3b2c3192d107aec"
     else
       url "#{base_url}/#{file_name}_darwin_amd64"
-      sha256 "80d0f6350c5d222259c22531fadddcca86339581efe455149b970648f5a2dd40"
+      sha256 "84dcbc6245940e5909243bdd93cd35ad2f08737b677a4a043b15f0d278b1c921"
     end
-  end
-
-  if OS.linux?
-    if `uname -m`.chomp  == "arm64"
-      url "#{base_url}/#{file_name}_linux_arm64"
-      sha256 "de357c92d62c4640da1c7750bbc70338c699a89f3ff4eab48c20a8e62aca1413"
-    else
-      url "#{base_url}/#{file_name}_linux_amd64"
-      sha256 "1b0f2f9a357cb930a4c256620018b25adb634547a8fb315ddc303f150d4cf7f3"
-    end 
+  else
+    url "#{base_url}/#{file_name}_linux_amd64"
+    sha256 "e51f45ae6708edf8ad6b980e0045b8fdb49b4d31f3e1a70106ade5d11d20c429"
   end
 
   def install
@@ -38,13 +31,8 @@ class Func < Formula
       else
         FileUtils.mv("func_darwin_amd64", "kn-func")
       end
-    end
-    if OS.linux?
-      if `uname -m`.chomp == "arm64"
-        FileUtils.mv("func_linux_arm64", "kn-func")
-      else  
-        FileUtils.mv("func_linux_amd64", "kn-func")
-      end  
+    else
+      FileUtils.mv("func_linux_amd64", "kn-func")
     end
     p "Installing kn-func binary in " + bin
     bin.install "kn-func"
