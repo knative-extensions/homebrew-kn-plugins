d64c7bdc65f39dcaaa89a808dc52fe1b5ca55a7f
Olivier Tardieu <tardieu@users.noreply.github.com>
2022-10-20 12:01:13 -0400

    Update quickstart formula to v1.8.1 (#122)

--- quickstart.rb
+++ quickstart.rb
@@ -1,7 +1,7 @@
 require 'fileutils'
 
 class Quickstart < Formula
-  v = "v1.8.0"
+  v = "v1.8.1"
   plugin_name = "quickstart"
   path_name = "kn-plugin-#{plugin_name}"
   file_name = "kn-#{plugin_name}"
