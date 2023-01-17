a010fba01f90ea2b250fd73b2d0864dca2b7313a
Lance Ball <lball@redhat.com>
2023-01-17 06:54:55 -0500

    chore(func): bump latest func to 1.8.1 (#127)
    * chore(func): bump latest func to 1.8.1
    
    This release contains the fix for https://github.com/knative/func/security/advisories/GHSA-5336-2g3f-9g3m
    
    /kind chore
    
    Signed-off-by: Lance Ball <lball@redhat.com>
    
    * fixup: bump to the new 1.8.2 version
    
    Signed-off-by: Lance Ball <lball@redhat.com>
    
    Signed-off-by: Lance Ball <lball@redhat.com>

--- func.rb
+++ func.rb
@@ -1,7 +1,7 @@
 require 'fileutils'
 
 class Func < Formula
-  v = "v1.8.0"
+  v = "v1.8.2"
   plugin_name = "func"
   path_name = "#{plugin_name}"
   file_name = "#{plugin_name}"
@@ -14,21 +14,21 @@ class Func < Formula
   if OS.mac?
     if `uname -m`.chomp  == "arm64"
       url "#{base_url}/#{file_name}_darwin_arm64"
-      sha256 "2dbb88b62b7f35c597f6c1bdbb8576be53aa49ac9978d2e6ae65e9b75246b8df"
+      sha256 "5ab2906f88029d707cf0289d0cc5d6c8e1334459c2e714839def8892f53114a5"
     else
       url "#{base_url}/#{file_name}_darwin_amd64"
-      sha256 "53d786383a1aa138e507cbb71b2d4c5ede5df601ae6d5f1a8316e17a13817f35"
+      sha256 "08051d854ac99cca0d94bea4c19bc9d80e1f867c9a7ca092a2cfa2912975391d"
     end
   end
 
   if OS.linux?
     if `uname -m`.chomp  == "arm64"
       url "#{base_url}/#{file_name}_linux_arm64"
-      sha256 "bf051268651ad5bed2f6298b6e6e4e3a422235a46fa3a4d731d1b47324118f82"
+      sha256 "f81ab52904a3a7ab34b83af15f44af11ab7a7c17292a779f66f16ef48e363ff1"
     else
       url "#{base_url}/#{file_name}_linux_amd64"
-      sha256 "a5010db6d20c76a078fc880bfa08be1a09cdddf8226095475c018ac3175a6ff4"
-    end 
+      sha256 "df33384c893a201473b004ca9f3eb78cd32b7981438da0da0de48e419203d103"
+    end
   end
 
   def install
@@ -42,9 +42,9 @@ class Func < Formula
     if OS.linux?
       if `uname -m`.chomp == "arm64"
         FileUtils.mv("func_linux_arm64", "kn-func")
-      else  
+      else
         FileUtils.mv("func_linux_amd64", "kn-func")
-      end  
+      end
     end
     p "Installing kn-func binary in " + bin
     bin.install "kn-func"
