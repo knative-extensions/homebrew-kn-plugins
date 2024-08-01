9a4f1968fc5a77480c2f9660db65c0733102e597
David Simansky <dsimansk@redhat.com>
2024-08-01 14:01:53 +0200

    Refactor kn-plugin-event to common formula format

--- event@1.13.rb
+++ event@1.13.rb
@@ -1,24 +1,43 @@
-require 'fileutils'
-require 'json'
-
-require_relative 'kn/plugin/event'
+# Generated through hack/update-codegen.sh. Don't edit manually.
+# Next line is used to identify version of the file.
+# event_version:1.13.0
+require "fileutils"
 
 class EventAT113 < Formula
-  PLUGIN = Kn::Plugin::Event.new 'v1.13.0'
-  
-  homepage PLUGIN.homepage
-  version PLUGIN.version
-  url PLUGIN.url
-  sha256 PLUGIN.sha256
+  homepage "https://github.com/knative-extensions/kn-plugin-event"
+
+  v = "knative-v1.13.0"
+  version v
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/knative-extensions/kn-plugin-event/releases/download/#{v}/kn-event-darwin-amd64"
+    sha256 "fc28b5ff9fc4cda44abe56c7e440da9cc2258f3b3fe0ffb060b2e4fd5d4fc890"
+  elsif OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/knative-extensions/kn-plugin-event/releases/download/#{v}/kn-event-darwin-arm64"
+    sha256 "d58473cfa684fa3f868cf37d98583a9a5327f874d9b249755c6e1a5fe035fce0"
+  elsif OS.linux? && Hardware::CPU.arm?
+    url "https://github.com/knative-extensions/kn-plugin-event/releases/download/#{v}/kn-event-linux-arm64"
+    sha256 "efe341fb9e7829f2e0a929f51450bc55b231e01cb30ec2b3bf4be398bb02bca5"
+  else
+    url "https://github.com/knative-extensions/kn-plugin-event/releases/download/#{v}/kn-event-linux-amd64"
+    sha256 "030f913bc8e533bd0cbee261bd08273f628d8b1792639e4e5146f5085d729d9a"
+  end
 
   def install
-    FileUtils.mv(PLUGIN.source_bin, PLUGIN.target_bin)
-    bin.install PLUGIN.target_bin
+    if OS.mac? && Hardware::CPU.intel?
+      FileUtils.mv("kn-event-darwin-amd64", "kn-event")
+    elsif OS.mac? && Hardware::CPU.arm?
+      FileUtils.mv("kn-event-darwin-arm64", "kn-event")
+    elsif OS.linux? && Hardware::CPU.arm?
+      FileUtils.mv("kn-event-linux-arm64", "kn-event")
+    else
+      FileUtils.mv("kn-event-linux-amd64", "kn-event")
+    end
+    bin.install "kn-event"
+    
   end
 
   test do
-    json = shell_output("#{bin}/#{PLUGIN.target_bin} version -o json").strip
-    version_info = JSON.parse(json)
-    assert_equal PLUGIN.version, version_info['version']
+    system "#{bin}/kn-event", "version"
   end
 end
