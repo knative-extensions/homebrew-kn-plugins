4b23b5f29884a57081d8b2e7c742affb1d94e1d0
dsimansk <5637792+dsimansk@users.noreply.github.com>
2025-01-27 14:27:49 +0000

    Update kn formula to new version

--- quickstart@1.16.rb
+++ quickstart@1.16.rb
@@ -1,26 +1,26 @@
 # Generated through hack/update-codegen.sh. Don't edit manually.
 # Next line is used to identify version of the file.
-# quickstart_version:1.17.0
+# quickstart_version:1.16.0
 require "fileutils"
 
-class Quickstart < Formula
+class QuickstartAT116 < Formula
   homepage "https://github.com/knative-extensions/kn-plugin-quickstart"
 
-  v = "knative-v1.17.0"
+  v = "knative-v1.16.0"
   version v
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/knative-extensions/kn-plugin-quickstart/releases/download/#{v}/kn-quickstart-darwin-amd64"
-    sha256 "b33b4e7305cd9a9e18eb8d83246b777eb4600b336930077a1109fd84bf9caab0"
+    sha256 "3ca4fb29283e8be7604c079c68f811f6df009a4e460194d6ca84f26276d563a3"
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/knative-extensions/kn-plugin-quickstart/releases/download/#{v}/kn-quickstart-darwin-arm64"
-    sha256 "3b6377649ebe26ea97a99d6607a29b161d3d697a5ddce28d55ade86c54110f6d"
+    sha256 "288b8ad5da8a17cdf3a111cef88929f711e69c41417aeb7b2327c452201f0dc5"
   elsif OS.linux? && Hardware::CPU.arm?
     url "https://github.com/knative-extensions/kn-plugin-quickstart/releases/download/#{v}/kn-quickstart-linux-arm64"
-    sha256 "69bae0d119b7c7712c225f9ea09aa22da6723bdaa1d901499bc6753801614d36"
+    sha256 "bffd382e319c6e78f96c6879f27dcbc4a3d04fc7259bb9328fe6c3e95805a343"
   else
     url "https://github.com/knative-extensions/kn-plugin-quickstart/releases/download/#{v}/kn-quickstart-linux-amd64"
-    sha256 "d55a009b11e028c57cbb3b111149159189e10eb841269e2022f2f8e35ee09f7c"
+    sha256 "50beae2561a38fa9f44301b2dfed1f36b78da11ffcb6250f98e84c750f8d5488"
   end
 
   def install
--- source-kafka@1.16.rb
+++ source-kafka@1.16.rb
@@ -1,26 +1,26 @@
 # Generated through hack/update-codegen.sh. Don't edit manually.
 # Next line is used to identify version of the file.
-# source-kafka_version:1.17.0
+# source-kafka_version:1.16.0
 require "fileutils"
 
-class SourceKafka < Formula
+class SourceKafkaAT116 < Formula
   homepage "https://github.com/knative-extensions/kn-plugin-source-kafka"
 
-  v = "knative-v1.17.0"
+  v = "knative-v1.16.0"
   version v
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/knative-extensions/kn-plugin-source-kafka/releases/download/#{v}/kn-source-kafka-darwin-amd64"
-    sha256 "6eafad8456e9ba9d2d24e2ab328923be665535670dc4b0e4267999726388ffb5"
+    sha256 "d2be0f6a39b7a4ed3bce8f03b6d77e62ed37ee02e8ed8b2c6632f7c55f636e43"
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/knative-extensions/kn-plugin-source-kafka/releases/download/#{v}/kn-source-kafka-darwin-arm64"
-    sha256 "de2a5f2f403d2d1f3ef2b1cdaaac00aa19299416f482f9e2c5948a725a7a63ad"
+    sha256 "97f88ee23d4f789201565f4a2e960bbfd7ec54542b51ad3bdd5f262c7e8e67d1"
   elsif OS.linux? && Hardware::CPU.arm?
     url "https://github.com/knative-extensions/kn-plugin-source-kafka/releases/download/#{v}/kn-source-kafka-linux-arm64"
-    sha256 "93879e4d15ee02318ab4a44ef4eabcb06609cd0e4e7b2c7b7284dec205ed8bec"
+    sha256 "2819288f2ad7e118b30a4323e8af8784e94bc5887db3a6e702712471683f0063"
   else
     url "https://github.com/knative-extensions/kn-plugin-source-kafka/releases/download/#{v}/kn-source-kafka-linux-amd64"
-    sha256 "6ef365e7dbd31906d2049b9e16e1e34c0d9dd0c45f9a5781f016e4bec22978af"
+    sha256 "90a7c792e3674fc7df0c35ef75afa417b00c07258c988d5fab83eabcce25e5c2"
   end
 
   def install
